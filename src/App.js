ad5158d7d2aedbbf6fae229453fcfa20ae8c0352
goodie007 <aruoturegoodness@gmail.com>
2022-01-13 15:45:20 +0100

    blockchain-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Status from '../src/components/Status'
+import Send from '../src/components/Send'
+import Transactions from '../src/components/Transactions'
+import axios from 'axios';
 
-function App() {
+const endpoint = '/mine_block'
+
+class App extends Component {
+  constructor(props){
+    super(props);
+  }
+  componentWillMount() {
+    axios.get(endpoint)
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Status/>
+    <Send/>
+    <Transactions/>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
